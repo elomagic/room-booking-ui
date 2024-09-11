5af96917291ad185cbe0992cd444efddf59aa92f
C. Rambow <rambow.dev@elomagic.de>
2024-09-11 20:21:27 +0200

    URL bug fixed

--- src/providers/EwsProxyProvider.ts
+++ src/providers/EwsProxyProvider.ts
@@ -2,6 +2,8 @@ import {Appointment, AppointmentProvider} from "./AppointmentProvider.ts";
 
 export class EwsProxyProvider extends AppointmentProvider {
 
+    baseUrl = localStorage.getItem("rb.ext.url") ?? "https://localhost";
+
     createHeaders(): Headers {
         // Since this request will send JSON data in the body,
         // we need to set the `Content-Type` header to `application/json`
@@ -10,14 +12,14 @@ export class EwsProxyProvider extends AppointmentProvider {
         // We also need to set the `Accept` header to `application/json`
         // to tell the server that we expect JSON in response
         headers.set('Accept', 'application/json')
-        headers.set('RB-ApiKey', localStorage.getItem("rb.ext.api") ?? "demo")
+        headers.set('RB-ApiKey', localStorage.getItem("rb.ext.apiKey") ?? "demo")
         headers.set('RB-Resource-ID', localStorage.getItem("rb.ext.resourceId") ?? "0")
 
         return headers;
     }
 
     createAdHocAppointment(durationInMinutes: number): Promise<boolean> {
-        const request: RequestInfo = new Request('/rest/appointment', {
+        const request: RequestInfo = new Request(this.baseUrl + '/api/appointment', {
             // We need to set the `method` to `POST` and assign the headers
             method: 'POST',
             headers: this.createHeaders(),
@@ -33,7 +35,7 @@ export class EwsProxyProvider extends AppointmentProvider {
     }
 
     extendCurrentAppointment(durationInMinutes: number): Promise<boolean> {
-        const request: RequestInfo = new Request('/rest/appointment', {
+        const request: RequestInfo = new Request(this.baseUrl + '/api/appointment', {
             // We need to set the `method` to `POST` and assign the headers
             method: 'PUT',
             headers: this.createHeaders(),
@@ -49,7 +51,7 @@ export class EwsProxyProvider extends AppointmentProvider {
     }
 
     getAppointmentsOfToday(): Promise<Appointment[]> {
-        const request: RequestInfo = new Request('/rest/appointment', {
+        const request: RequestInfo = new Request(this.baseUrl + '/api/appointment', {
             // We need to set the `method` to `POST` and assign the headers
             method: 'GET',
             headers: this.createHeaders()
@@ -60,7 +62,7 @@ export class EwsProxyProvider extends AppointmentProvider {
     }
 
     terminateCurrentAppointment(): Promise<boolean> {
-        const request: RequestInfo = new Request('/rest/appointment', {
+        const request: RequestInfo = new Request(this.baseUrl + '/api/appointment', {
             // We need to set the `method` to `POST` and assign the headers
             method: 'PUT',
             headers: this.createHeaders()
