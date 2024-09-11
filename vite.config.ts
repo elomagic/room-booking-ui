45a1799227a173b0ab1d0bce13ddb075c46b1d70
C. Rambow <rambow.dev@elomagic.de>
2024-09-11 21:03:44 +0200

    ZIP support added

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {nodePolyfills} from "vite-plugin-node-polyfills";
+import zipPack from "vite-plugin-zip-pack";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         nodePolyfills(),
-        react()
+        react(),
+        zipPack({
+            outFileName: 'room-booking-ui.zip',
+        })
     ],
 })
